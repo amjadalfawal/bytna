2b8aa6cb19b847630c6f3ae7642af1ea0ca6c01e
amjad alfawal <it.eng.amjadalfawal@gmail.com>
2022-12-14 02:09:30 +0300

    Update __manifest__.py

--- tecnote_internal_transfer_customization/__manifest__.py
+++ tecnote_internal_transfer_customization/__manifest__.py
@@ -7,20 +7,11 @@
     """,
     'author': "Tecnote Technology",
     'website': "https://www.tecnote.ca/",
-    'depends': ['stock','stock_barcode'],
+    'depends': ['stock','product_expiry','stock_barcode'],
 
     'data': [
         'views/stock_production_lot.xml',
         'views/stock_picking.xml',
         'report/product_label.xml'
-        # 'report/delivery_report.xml',
     ],
-    # 'assets': {
-    #     'web.assets_backend': [
-    #         'tecnote_internal_transfer_customization/static/src/**/*.js',
-    #     ],
-    #     'web.assets_qweb': [
-    #         'tecnote_internal_transfer_customization/static/src/**/*.xml',
-    #     ],
-    # }
 }
